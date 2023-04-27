069662e53bcf1d0661be679b26674125b51606c3
Artur Henrique <arthenrdev@gmail.com>
2023-04-27 14:36:43 -0300

    feat: Req 04 - Criação validações produtos

---
+++ src/middlewares/index.js
@@ -0,0 +1,4 @@
+// Validate Products
+const validateProductsName = require('./validateProductsName');
+
+module.exports = { validateProductsName };
--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { validateProductsName } = require('../middlewares');
 const productsController = require('../controllers/products.controller');
 
 const productsRoute = express.Router();
@@ -7,6 +8,6 @@ productsRoute.get('/', productsController.getAllProducts);
 
 productsRoute.get('/:id', productsController.getProductById);
 
-productsRoute.post('/', productsController.createProduct);
+productsRoute.post('/', validateProductsName, productsController.createProduct);
 
 module.exports = productsRoute;

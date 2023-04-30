703d39849ddc1b27a8dd3148897b5f55d4ae0453
Artur Henrique <arthenrdev@gmail.com>
2023-04-30 20:27:27 -0300

    Refatoração arquivo products.service.js

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -1,18 +1,9 @@
 const productsModel = require('../models/products.model');
 
-const getAllProducts = async () => {
-  const products = await productsModel.getAllProducts();
-  return products;
-};
+const getAllProducts = async () => productsModel.getAllProducts();
 
-const getProductById = async (Productid) => {
-  const products = await productsModel.getProductById(Productid);
-  return products;
-};
+const getProductById = async (Productid) => productsModel.getProductById(Productid);
 
-const createProduct = async (product) => {
-  const products = await productsModel.createProduct(product);
-  return products;
-};
+const createProduct = async (product) => productsModel.createProduct(product);
 
 module.exports = { getAllProducts, getProductById, createProduct };
